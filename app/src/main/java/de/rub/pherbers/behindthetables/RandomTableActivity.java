1491c1c40f706287d193c92601ce343612f69be8
Patrick Herbers <patrick.herbers@rub.de>
2017-03-21 19:55:53 +0100

    Why is this not pushed?

--- app/src/main/java/de/rub/pherbers/behindthetables/RandomTableActivity.java
+++ app/src/main/java/de/rub/pherbers/behindthetables/RandomTableActivity.java
@@ -1,23 +1,19 @@
 package de.rub.pherbers.behindthetables;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SimpleItemAnimator;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.webkit.URLUtil;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -29,16 +25,16 @@ import de.rub.pherbers.behindthetables.data.TableCollectionContainer;
 import de.rub.pherbers.behindthetables.data.TableFile;
 import de.rub.pherbers.behindthetables.data.TableReader;
 import de.rub.pherbers.behindthetables.sql.DBAdapter;
-import de.rub.pherbers.behindthetables.view.RandomTableView;
+import de.rub.pherbers.behindthetables.view.RandomTableViewHolder;
 import timber.log.Timber;
 
 public class RandomTableActivity extends AppCompatActivity {
 
-	public static final String EXTRA_TABLE_DATABASE_ID = BehindTheTables.APP_TAG + "extra_table_database_id";
+    public static final String EXTRA_TABLE_DATABASE_ID = BehindTheTables.APP_TAG + "extra_table_database_id";
 
-	private TableCollection table;
-	private RecyclerView listView;
-	private RandomTableListAdapter listAdapter;
+    private TableCollection table;
+    private RecyclerView listView;
+    private RandomTableListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,41 +46,40 @@ public class RandomTableActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-		Intent sourceIntent = getIntent();
-		if (!sourceIntent.hasExtra(EXTRA_TABLE_DATABASE_ID)) {
-			Timber.e("This Random Table activity has no Table Database ID in the start intent! Aborting!");
-			finish();
-			return;
-		}
+        Intent sourceIntent = getIntent();
+        if (!sourceIntent.hasExtra(EXTRA_TABLE_DATABASE_ID)) {
+            Timber.e("This Random Table activity has no Table Database ID in the start intent! Aborting!");
+            finish();
+            return;
+        }
 
-		long databaseID = sourceIntent.getLongExtra(EXTRA_TABLE_DATABASE_ID, -1);
-		DBAdapter adapter = new DBAdapter(this).open();
-		TableFile file = TableFile.getFromDB(databaseID, adapter);
-		adapter.close();
+        long databaseID = sourceIntent.getLongExtra(EXTRA_TABLE_DATABASE_ID, -1);
+        DBAdapter adapter = new DBAdapter(this).open();
+        TableFile file = TableFile.getFromDB(databaseID, adapter);
+        adapter.close();
 
-		if (file == null) {
-			Timber.e("Failed to optain a table file from the DB with the ID " + databaseID + "! Aborting activity!");
-			finish();
-			return;
-		}
+        if (file == null) {
+            Timber.e("Failed to optain a table file from the DB with the ID " + databaseID + "! Aborting activity!");
+            finish();
+            return;
+        }
 
-		String tableIdentifier = file.getIdentifier();
-		TableCollectionContainer tableCollectionContainer = TableCollectionContainer.getTableCollectionContainer();
-		if (!tableCollectionContainer.containsKey(tableIdentifier)) {
-			try {
-				if (file.isExternal()) {
-					//TODO Load external file
-				} else {
-					table = TableReader.readTable(getResources().openRawResource(file.getResourceID()));
-				}
-				tableCollectionContainer.put(tableIdentifier, table);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			table = tableCollectionContainer.get(tableIdentifier);
-		}
-
+        String tableIdentifier = file.getIdentifier();
+        TableCollectionContainer tableCollectionContainer = TableCollectionContainer.getTableCollectionContainer();
+        if (!tableCollectionContainer.containsKey(tableIdentifier)) {
+            try {
+                if (file.isExternal()) {
+                    //TODO Load external file
+                } else {
+                    table = TableReader.readTable(getResources().openRawResource(file.getResourceID()));
+                }
+                tableCollectionContainer.put(tableIdentifier, table);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            table = tableCollectionContainer.get(tableIdentifier);
+        }
 
 
         listView = (RecyclerView) findViewById(R.id.random_table_list);
@@ -96,8 +91,7 @@ public class RandomTableActivity extends AppCompatActivity {
         DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
         listView.addItemDecoration(mDividerItemDecoration);
 
-        SimpleItemAnimator animator = new MyItemAnimator();
-        animator = new DefaultItemAnimator();
+        RecyclerView.ItemAnimator animator = new DefaultItemAnimator();
         listView.setItemAnimator(animator);
 
         TextView tv = (TextView) findViewById(R.id.random_table_title);
@@ -114,8 +108,8 @@ public class RandomTableActivity extends AppCompatActivity {
 
     public void diceRollAction(View mView) {
         //table.rollAllTables();
-        for(int i = 0; i  < listAdapter.getItemCount(); i++) {
-            RandomTableViewHolder v = (RandomTableViewHolder)listView.findViewHolderForAdapterPosition(i);
+        for (int i = 0; i < listAdapter.getItemCount(); i++) {
+            RandomTableViewHolder v = (RandomTableViewHolder) listView.findViewHolderForAdapterPosition(i);
             int prev = table.getTables().get(i).getRolledIndex();
             table.getTables().get(i).roll();
             if (v != null) {
@@ -135,45 +129,45 @@ public class RandomTableActivity extends AppCompatActivity {
     }
 
     public void actionCollapseAll() {
-        for(int i = 0; i < listAdapter.getItemCount(); i++) {
+        for (int i = 0; i < listAdapter.getItemCount(); i++) {
             RecyclerView.ViewHolder v = listView.findViewHolderForLayoutPosition(i);
             if (v == null) {
                 continue;
             }
-            if(v instanceof RandomTableViewHolder)
+            if (v instanceof RandomTableViewHolder)
                 ((RandomTableViewHolder) v).collapse(false);
         }
-        for(RandomTable t:table.getTables())
+        for (RandomTable t : table.getTables())
             t.setExpanded(false);
-        int firstPos = ((LinearLayoutManager)listView.getLayoutManager()).findFirstVisibleItemPosition();
-        int lastPos = ((LinearLayoutManager)listView.getLayoutManager()).findLastVisibleItemPosition();
-        if(firstPos > 0) {
+        int firstPos = ((LinearLayoutManager) listView.getLayoutManager()).findFirstVisibleItemPosition();
+        int lastPos = ((LinearLayoutManager) listView.getLayoutManager()).findLastVisibleItemPosition();
+        if (firstPos > 0) {
             listAdapter.notifyItemRangeChanged(0, firstPos);
         }
-        if(lastPos < listAdapter.getItemCount()-1) {
-            listAdapter.notifyItemRangeChanged(lastPos+1, listAdapter.getItemCount()-1);
+        if (lastPos < listAdapter.getItemCount() - 1) {
+            listAdapter.notifyItemRangeChanged(lastPos + 1, listAdapter.getItemCount() - 1);
         }
         //listView.smoothScrollToPosition(0);
     }
 
     public void actionExpandAll() {
-        for(int i = 0; i < listAdapter.getItemCount(); i++) {
+        for (int i = 0; i < listAdapter.getItemCount(); i++) {
             RecyclerView.ViewHolder v = listView.findViewHolderForLayoutPosition(i);
             if (v == null) {
                 continue;
             }
-            if(v instanceof RandomTableViewHolder)
+            if (v instanceof RandomTableViewHolder)
                 ((RandomTableViewHolder) v).expand(false);
         }
-        for(RandomTable t:table.getTables())
+        for (RandomTable t : table.getTables())
             t.setExpanded(true);
-        int firstPos = ((LinearLayoutManager)listView.getLayoutManager()).findFirstVisibleItemPosition();
-        int lastPos = ((LinearLayoutManager)listView.getLayoutManager()).findLastVisibleItemPosition();
-        if(firstPos > 0) {
+        int firstPos = ((LinearLayoutManager) listView.getLayoutManager()).findFirstVisibleItemPosition();
+        int lastPos = ((LinearLayoutManager) listView.getLayoutManager()).findLastVisibleItemPosition();
+        if (firstPos > 0) {
             listAdapter.notifyItemRangeChanged(0, firstPos);
         }
-        if(lastPos < listAdapter.getItemCount()-1) {
-            listAdapter.notifyItemRangeChanged(lastPos+1, listAdapter.getItemCount()-1);
+        if (lastPos < listAdapter.getItemCount() - 1) {
+            listAdapter.notifyItemRangeChanged(lastPos + 1, listAdapter.getItemCount() - 1);
         }
     }
 
@@ -193,7 +187,7 @@ public class RandomTableActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
@@ -219,7 +213,7 @@ public class RandomTableActivity extends AppCompatActivity {
     }
 
     public void resetTable() {
-        for(int i = 0; i  < listAdapter.getItemCount(); i++) {
+        for (int i = 0; i < listAdapter.getItemCount(); i++) {
             table.getTables().get(i).setRolledIndex(-1);
             redrawListAtPos(i);
         }
@@ -229,7 +223,7 @@ public class RandomTableActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.random_table_activity_menu, menu);
         // Disable "Open on reddit" if no reference is given
-        if(!URLUtil.isValidUrl(table.getReference())) {
+        if (!URLUtil.isValidUrl(table.getReference())) {
             MenuItem item = menu.findItem(R.id.random_table_activity_reference);
             item.setVisible(false);
             item.setEnabled(false);
