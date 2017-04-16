da4adbd469f49a30295c5b55dd670fd1da522292
Patrick Herbers <patrick.herbers@rub.de>
2017-04-16 23:35:20 +0200

    Switched TableReader to GSON

--- app/src/main/java/de/rub/pherbers/behindthetables/data/TableReader.java
+++ app/src/main/java/de/rub/pherbers/behindthetables/data/TableReader.java
@@ -2,6 +2,11 @@ package de.rub.pherbers.behindthetables.data;
 
 import android.util.JsonReader;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,124 +20,78 @@ import java.util.List;
 public abstract class TableReader {
 
     public static TableCollection readTable(InputStream is) throws IOException {
+        String title;
+        List<RandomTable> tables;
+        String reference;
+        String description;
+        String id;
+        String category;
+        List<TableLink> related_tables;
+        List<TableLink> use_with_tables;
+        List<String> keywords = new ArrayList<>();
 
-        JsonReader jr = new JsonReader(new InputStreamReader(is, "UTF-8"));
-        jr.beginObject();
-
-        String title = "Default";
-        List<RandomTable> tables = null;
-        String reference = "";
-        String description = "";
-        String id = "";
-        String category = "No Category";
-        List<TableLink> related_tables = null;
-        List<TableLink> use_with_tables = null;
-        List<String> keywords = null;
-
-        while (jr.hasNext()) {
-            String name = jr.nextName();
-            if (name.equals("title"))
-                title = jr.nextString();
-            else if (name.equals("category"))
-                category = jr.nextString();
-            else if (name.equals("tables"))
-                tables = readTableListJson(jr);
-            else if (name.equals("description")) {
-                description = jr.nextString();
-            } else if (name.equals("id")) {
-                id = jr.nextString();
-            } else if (name.equals("keywords")) {
-                jr.beginArray();
-                keywords = new ArrayList<>();
-                while (jr.hasNext())
-                    keywords.add(jr.nextString());
-                jr.endArray();
-            } else if (name.equals("reference")) {
-                reference = jr.nextString();
-            } else if (name.equals("related_tables")) {
-                related_tables = readLinks(jr);
-            } else if (name.equals("use_with")) {
-                use_with_tables = readLinks(jr);
-            }
+        JsonParser parser = new JsonParser();
+        JsonObject tableCollection = parser.parse(new InputStreamReader(is, "UTF-8")).getAsJsonObject();
+        is.close();
+        title = tableCollection.get("title").getAsString();
+        description = tableCollection.get("description").getAsString();
+        reference = tableCollection.get("reference").getAsString();
+        category = tableCollection.get("category").getAsString();
+        id = tableCollection.get("id").getAsString();
+        JsonArray keywordsArray = tableCollection.get("keywords").getAsJsonArray();
+        for (JsonElement e: keywordsArray) {
+            keywords.add(e.getAsString());
         }
-        jr.endObject();
+        related_tables = readLinks(tableCollection.get("related_tables").getAsJsonArray());
+        use_with_tables = readLinks(tableCollection.get("use_with").getAsJsonArray());
+
+        tables = readTableListJson(tableCollection.get("tables").getAsJsonArray());
 
         TableCollection tc = new TableCollection(title, tables, reference, description, category, id, related_tables, use_with_tables, keywords);
 
-        is.close();
 
         return tc;
     }
 
-    private static List<TableLink> readLinks(JsonReader jr) throws IOException {
+    private static List<TableLink> readLinks(JsonArray ja) throws IOException {
         List<TableLink> tableLinks = new ArrayList<>();
-        jr.beginArray();
-        while (jr.hasNext()) {
-            jr.beginObject();
-            String link = "";
-            String title = "";
-            while (jr.hasNext()) {
-                String name = jr.nextName();
-                if (name.equals("link"))
-                    link = jr.nextString();
-                else if (name.equals("title"))
-                    title = jr.nextString();
-            }
+        for(JsonElement e: ja) {
+            String title = e.getAsJsonObject().get("title").getAsString();
+            String link = e.getAsJsonObject().get("link").getAsString();
             tableLinks.add(new TableLink(link, title));
-            jr.endObject();
         }
-        jr.endArray();
         return tableLinks;
     }
 
-    private static List<RandomTable> readTableListJson(JsonReader jr) throws IOException {
-        jr.beginArray();
-        List<RandomTable> tables = new ArrayList<>();
+    private static List<RandomTable> readTableListJson(JsonArray ja) throws IOException {
         int i = 0;
-        while (jr.hasNext()) {
-            tables.add(readTableJson(jr, i));
-            i++;
+        List<RandomTable> tables = new ArrayList<>();
+        for(JsonElement e: ja) {
+            JsonObject jo = e.getAsJsonObject();
+            if(jo.has("subcategory")) {
+
+            } else {
+                tables.add(readTableJson(jo, i));
+                i++;
+            }
         }
-        jr.endArray();
         return tables;
     }
 
-    private static RandomTable readTableJson(JsonReader jr, int index) throws IOException {
-        String title = "Table";
-        String dice = "xdy";
+    private static RandomTable readTableJson(JsonObject jo, int index) throws IOException {
+        String title;
+        String dice;
         List<TableEntry> entries = new ArrayList<>();
 
-        jr.beginObject();
-
-        while (jr.hasNext()) {
-            String name = jr.nextName();
-            if (name.equals("name"))
-                title = jr.nextString();
-            else if (name.equals("dice"))
-                dice = jr.nextString();
-            else if (name.equals("table_entries")) {
-                int i = 1; //TODO is this ever used?
-                jr.beginArray();
-                while (jr.hasNext()) {
-                    jr.beginObject();
-                    String entry = "";
-                    String diceVal = "";
-                    while (jr.hasNext()) {
-                        String entry_key = jr.nextName();
-                        if (entry_key.equals("dice_val")) {
-                            diceVal = jr.nextString();
-                        } else if (entry_key.equals("entry")) {
-                            entry = jr.nextString();
-                        }
-                    }
-                    entries.add(new TableEntry(entry, diceVal));
-                    i++;
-                    jr.endObject();
-                }
-                jr.endArray();
-            }
+        title = jo.get("name").getAsString();
+        dice = jo.get("dice").getAsString();
+        JsonArray table_entries = jo.get("table_entries").getAsJsonArray();
+        for(JsonElement e: table_entries) {
+            String entry = e.getAsJsonObject().get("entry").getAsString();
+            String diceVal = e.getAsJsonObject().get("dice_val").getAsString();
+            entries.add(new TableEntry(entry, diceVal));
         }
-        jr.endObject();
+
         RandomTable table = new RandomTable(title, dice, index, entries);
         return table;
     }
